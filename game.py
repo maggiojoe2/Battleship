71e45c5bbbd4a70356df474b797925bbdb31975e
Maggio <jmaggio@hq.corp.viasat.com>
2016-06-02 11:15:44 -0500

    Added game.py for the play_game function. Fixed a bug in print board where the printed board was being stored in the board variable.

---
+++ game.py
@@ -0,0 +1,37 @@
+from board import *
+def play_game():
+	game_over = 'n'
+	ai_board = create_game_board()
+	#print_board(ai_board) #FIXME
+
+	user_board = create_board()
+	print('   Here is your game board used for finding battleships: \n')
+	print_board(user_board)
+	while game_over != 'y':
+		guess_row = len(ai_board)
+		guess_col = len(ai_board)
+		print(' ')
+		#get user input and make sure it isn't invalid (outside board or already guessed)
+		while guess_row not in range(len(ai_board)) or guess_col not in range(len(ai_board)):
+			guess_row = int(input('   Input row to fire at: '))
+			guess_col = int(input('   Input column to fire at: '))
+			ai_location = ai_board[guess_row][guess_col]
+			user_location = user_board[guess_row][guess_col]
+			if guess_row not in range(len(ai_board)) or guess_col not in range(len(ai_board)):
+				print('   Oops, that\'s not even in the ocean!')
+			elif user_location is not '~':
+				print('   You have already fired there.')
+				guess_row = len(ai_board)
+				guess_col = len(ai_board)
+
+		print(ai_board[guess_row][guess_col])
+		if ai_location is not '~':
+			print('\n   Hit!\n')
+			user_board[guess_row][guess_col] = 'X'
+		else:
+			print('\n   Miss\n')
+			user_board[guess_row][guess_col] = 'O'
+		print_board(user_board)
+		game_over = input('Enter \"y\" to quit: ')
+
+
